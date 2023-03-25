7893b15695e42310d7bc359d55ffd872cf04817a
mhaneefleghari9 <m.haneef1966@gmail.com>
2023-03-26 01:08:30 +0500

    git changes all

--- searchingapp/urls.py
+++ searchingapp/urls.py
@@ -6,6 +6,8 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
 
     path('searching/', views.keyword_searching, name='searching'),
+    path('blog/', views.blog, name='blog'),
+    path('home/', views.home, name='home'),
     path('login/', login_view, name="login"),
     path('register/', register_user, name="register"),
     path("logout/", LogoutView.as_view(), name="logout")
