9967ebc4e84406f5b8f2d381d4075bfd2847389c
mhaneefleghari9 <m.haneef1966@gmail.com>
2023-04-08 13:09:10 +0500

    add authentication

--- searchingapp/urls.py
+++ searchingapp/urls.py
@@ -1,5 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, re_path
 from . import views
+from django.contrib.auth import views as auth_views
 from .views import login_view, register_user
 from django.contrib.auth.views import LogoutView
 
@@ -10,5 +13,15 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('login/', login_view, name="login"),
     path('register/', register_user, name="register"),
-    path("logout/", LogoutView.as_view(), name="logout")
+    # path('logout/', views.logout_view, name='logout'),
+    path('change_password/', views.change_password, name='change_password'),
+    # path('forgot/', views.forgot_password, name='forgot_password'),
+    # path('reset/<str:reset_token>/', views.reset_password, name='reset_password'),
+    path('signout/', views.logout_view, name='signout'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
 ]
+
--- searchingapp/views.py
+++ searchingapp/views.py
@@ -1,22 +1,30 @@
 import xmltodict
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.contrib.sites import requests
-from django.http import HttpResponse
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from .forms import LoginForm, SignUpForm
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.urls import reverse_lazy, reverse
+from django.utils import timezone
+
+from .forms import LoginForm, SignUpForm, CustomPasswordChangeForm
+from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.decorators import login_required
 from .models import Blog
 
 
-# @login_required(login_url="/login/")
+# @login_required(login_url="login")
 def keyword_searching(request):
     if request.method == "POST":
         keyword_searching = request.POST.get('keyword_searching')
         country_select = request.POST.get('country_select')
         headers = {'Content-Type': 'application/json', 'Authorization': 'Basic'}
-        url = "https://google.com/complete/search?output=toolbar&gl=" + \
+        url = "https://api.keywordtool.io/v2/keyword_searching/volume/google" + \
               country_select + "&q=" + keyword_searching
 
         r = requests.get(url, headers=headers)
@@ -35,6 +43,7 @@ def keyword_searching(request):
     return HttpResponse(html_template.render(context, request))
 
 
+@login_required(login_url="register")
 def login_view(request):
     form = LoginForm(request.POST or None)
 
@@ -66,13 +75,15 @@ def register_user(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
+            email = form.cleaned_data.get("email")
+            number = form.cleaned_data.get("number")
             raw_password = form.cleaned_data.get("password1")
-            user = authenticate(username=username, password=raw_password)
+            user = authenticate(username=username, email=email, number=number, password=raw_password)
 
             msg = 'User created successfully.'
             success = True
 
-            # return redirect("/login/")
+            return redirect("/login/")
 
         else:
             msg = 'Form is not valid'
@@ -82,11 +93,86 @@ def register_user(request):
     return render(request, "accounts/register.html", {"form": form, "msg": msg, "success": success})
 
 
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+
+# Forgot Password
+# def password_reset(request):
+#     if request.method == 'POST':
+#         email = request.POST.get('email')
+#         try:
+#             user = User.objects.get(email=email)
+#         except User.DoesNotExist:
+#             pass
+#         else:
+#             token = generate_unique_token()
+#             user.password_reset_token = token
+#             user.password_reset_token_created_at = timezone.now()
+#             user.save()
+#             send_password_reset_email(user.email, token)
+#     return render(request, 'password_reset.html')
+
+
+def password_reset_confirm(request, token):
+    try:
+        user = User.objects.get(password_reset_token=token)
+    except User.DoesNotExist:
+        pass
+    else:
+        if user.password_reset_token_created_at < timezone.now() - timezone.timedelta(hours=24):
+            pass
+        else:
+            if request.method == 'POST':
+                password = request.POST.get('password')
+                user.set_password(password)
+                user.password_reset_token = ''
+                user.password_reset_token_created_at = None
+                user.save()
+    return render(request, 'password_reset_confirm.html')
+
+# Reset password
+from django.contrib import messages
+
+
+def password_reset(request):
+    if request.method == 'POST':
+        form = PasswordResetForm(request.POST)
+        if form.is_valid():
+            form.save(
+                request=request,
+                from_email=request.POST.get('email'),
+                email_template_name='password_reset_email.html',
+                subject_template_name='password_reset_subject.txt',
+            )
+            messages.success(request, 'An email has been sent to you with instructions on how to reset your password.')
+            return redirect('login')
+    else:
+        form = PasswordResetForm()
+    return render(request, 'password_reset_form.html', {'form': form})
+
+
+# Change password
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = CustomPasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('profile')
+    else:
+        form = CustomPasswordChangeForm(user=request.user)
+    return render(request, 'change_password.html', {'form': form})
+
+
 def blog(request):
-    blog = Blog.objects.all()
-    context = {'segment': blog}
+    blogs = Blog.objects.all()
+    context = {'blogs': blogs}
     html_template = loader.get_template('includes/blog.html')
-    return HttpResponse(html_template.render(context,request))
+    return HttpResponse(html_template.render(context, request))
 
 
 def home(request):
